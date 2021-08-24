f96ff7a87fc705a0a2e25d32d8a3072e78a830d2
Thomas Dokas <dokastho@umich.edu>
2021-08-24 15:24:39 -0400

    this one's quick and easy

--- fib.cpp
+++ fib.cpp
@@ -1,5 +1,22 @@
-#include "fib.h"
+#include <vector>
+#include <iostream>
 
-int main() {
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc > 2)
+    {
+        return 1;
+    }
+    
+    int fibSeq = atoi(argv[1]);
+    vector<uint64_t> memo;
+    memo.push_back((uint64_t)0);
+    memo.push_back((uint64_t)1);
+    for (int i = 2; i < fibSeq; i++)
+    {
+        memo.push_back(memo[i - 1] + memo[i - 2]);
+    }
+    cout << memo[fibSeq - 1] << "\n";
     return 0;
 }
--- fib.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIB_H
-#define FIB_H
-
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
-#endif
